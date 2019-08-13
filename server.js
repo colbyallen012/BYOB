917db5e1546b954df73184e9c010d70fc0fd31cd
Colby Allen <colbyallen012@gmail.com>
2019-08-13 13:01:09 -0600

    setup static file and babel

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ app.set('port', process.env.PORT || 3000)
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
-app.locals.title = 'Pet Box';
+app.locals.title = 'BYOB Parks';
 app.locals.parks = [
   { id: '1', name: 'Weir Gulch South', park_type: 'park' },
   { id: '2', name: 'Aztlan', park_type: 'park' },
   { id: '3', name: 'Lowry Open Space', park_type: 'open space' }
 ];
 
+app.get('/', (request, response) => {
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}`)
+})
 
