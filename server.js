95b311caee99ec27d7d8fe359ba9dac1dad96725
Colby Allen <colbyallen012@gmail.com>
2019-08-13 13:23:57 -0600

    get specific parks by park class

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.static('public'));
 app.use(cors());
 app.locals.title = 'BYOB Parks';
 app.locals.parks = [
-  { id: '1', name: 'Weir Gulch South', park_type: 'park' },
-  { id: '2', name: 'Aztlan', park_type: 'park' },
-  { id: '3', name: 'Lowry Open Space', park_type: 'open space' }
+  { id: '1', name: 'Weir Gulch South', park_type: 'park', park_class: 'linear' },
+  { id: '2', name: 'Aztlan', park_type: 'park', park_class: 'neighborhood'  },
+  { id: '3', name: 'Lowry Open Space', park_type: 'open space', park_class: 'special use'  }
 ];
 
 app.get('/', (request, response) => {
@@ -18,6 +18,13 @@ app.get('/', (request, response) => {
 
 app.get('/api/v1/parks', (request, response) => {
   const parks = app.locals.parks
+
+  return response.status(200).json({parks})
+})
+
+app.get('/api/v1/parks/park_class/neighborhood', (request, response) => {
+  const parkClass = 'neighborhood'
+  const parks = app.locals.parks.filter(park => park.park_class === parkClass)
 
   return response.status(200).json({parks})
 })
