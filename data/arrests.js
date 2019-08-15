96776f23a95d8981f63cb916c1b7eeb53bbc8ee8
Colby Allen <colbyallen012@gmail.com>
2019-08-14 18:42:53 -0600

    add team and arrest files

---
+++ data/arrests.js
@@ -0,0 +1,213 @@
+[
+  {
+  team_name: "Denver Broncos",
+  player: "Chad Kelly",
+  position: "QB",
+  category: "Trespassing",
+  description: "The Englewood Colorado Police arrested and charged Kelly with first-degree criminal trespassing after a report of a man allegedly inside a home."
+  },
+  {
+  team_name: "Denver Broncos",
+  player: "Adam Gotsis",
+  position: "DE",
+  category: "Sexual Assault",
+  description: "Accused of rape in an incident that happened in 2013 when Gotsis was a student at Georgia Tech."
+  },
+  {
+  team_name: "Denver Broncos",
+  player: "Carlos Henderson",
+  position: "WR",
+  category: "Drugs",
+  description: "Pulled over for speeding, cops found marijuana in the car."
+  },
+  {
+  team_name: "Minnesota Vikings",
+  player: "Cayleb Jones",
+  position: "WR",
+  category: "Domestic Assault",
+  description: "Accused of misdemeanor domestic assault, theft and interfering with a 911 phone call."
+  },
+  {
+  team_name: "Minnesota Vikings",
+  player: "Isame Faciane",
+  position: "OG",
+  category: "DUI",
+  description: "Charged with drunken driving after being pulled over for driving around barricades."
+  },
+  {
+  team_name: "Minnesota Vikings",
+  player: "Jabari Price",
+  position: "CB",
+  category: "DUI",
+  description: "Pulled over for speeding, accused of misdemeanor drunken driving."
+  },
+  {
+  team_name: "Cincinnati Bengals",
+  player: "Adam Jones",
+  position: "CB",
+  category: "Assault",
+  description: "Accused of poking hotel security employee in the eye in Cincinnati, obstructing police."
+  },
+  {
+  team_name: "Cincinnati Bengals",
+  player: "Orson Charles",
+  position: "FB",
+  category: "Gun",
+  description: "Accused of brandishing gun in car in apparent road rage incident."
+  },
+  {
+  team_name: "Cincinnati Bengals",
+  player: "Adam Jones",
+  position: "CB",
+  category: "Disorderly conduct",
+  description: "He was the passenger in a car pulled over for speeding and was cited for disorderly conduct. The driver was charged with drunk driving."
+  },
+  {
+  team_name: "Jacksonville Jaguars",
+  player: "Leonard Fournette",
+  position: "RB",
+  category: "Suspended License",
+  description: "Failed to pay fine from November citation"
+  },
+  {
+  team_name: "Jacksonville Jaguars",
+  player: "Barry Church",
+  position: "S",
+  category: "Disorderly Conduct",
+  description: "Church and 3 of his teammates were detained after allegedly a fight broke out over a $64k bar tab that they believed was complementary. The bill was paid and reportedly no charges were filed."
+  },
+  {
+  team_name: "Jacksonville Jaguars",
+  player: "Ronnie Harrison",
+  position: "S",
+  category: "Disorderly Conduct",
+  description: "Harrison and 3 of his teammates were detained after allegedly a fight broke out over a $64k bar tab that they believed was complementary. The bill was paid and reportedly no charges were filed."
+  },
+  {
+  team_name: "Tampa Bay Buccaneers",
+  player: "TJ Ward",
+  position: "S",
+  category: "Outstanding Warrant",
+  description: "TJ Ward was arrested following an October drug possession arrest for an outstanding warrant related to that case."
+  },
+  {
+  team_name: "Tampa Bay Buccaneers",
+  player: "Austin Seferian-Jenkins",
+  position: "TE",
+  category: "DUI",
+  description: "Pulled over for speeding and driving erratically, accused of drunken driving in Tampa."
+  },
+  {
+  team_name: "Tampa Bay Buccaneers",
+  player: "Akeem Spence",
+  position: "DT",
+  category: "Drugs",
+  description: "Pulled over for speeding in Alabama, accused of marijuana possession after police dog searched car."
+  },
+
+{
+  team_name: "Tennessee Titans",
+  player: "Dorial Green-Beckham",
+  position: "WR",
+  category: "Outstanding warrant",
+  description: "Pulled over for speeding in Missouri, where officer found an outstanding warrant for previous unpaid speeding ticket."
+  },
+  {
+  team_name: "Tennessee Titans",
+  player: "Justin Hunter",
+  position: "WR",
+  category: "Assault",
+  description: "Charged with felonious assault stemming from an incident at a Virginia Beach bar July 3."
+  },
+  {
+  team_name: "Cleveland Browns",
+  player: "Antonio Callaway",
+  position: "WR",
+  category: "Drugs",
+  description: "Pulled over for traffic violation in Ohio, accused of marijuana possession and driving with suspended license."
+  },
+  {
+  team_name: "Cleveland Browns",
+  player: "T.Y. McGill",
+  position: "DT",
+  category: "Drugs",
+  description: "Accused of marijuana possession at a hotel in Charlotte, NC.",
+  },
+  {
+  team_name: "Indianapolis Colts",
+  player: "David Parry",
+  position: "DT",
+  category: "DUI, resisting arrest",
+  description: "Suspected of stealing golf cart, driving drunk, resisting arrest in Scottsdale, Ariz."
+  },
+  {
+  team_name: "Indianapolis Colts",
+  player: "Robert Turbin",
+  position: "RB",
+  category: "Drugs",
+  description: "Police in Utah accused him of marijuana possession after pulling him over for running a stop sign and then smelling it in his car."
+  },
+  {
+  team_name: "Kansas City Chiefs",
+  player: "Kevin Pierre-Louis ",
+  position: "LB",
+  category: "Drugs",
+  description: "Was allegedly arrested in Johnson County, Kansas after a traffic stop for not displaying a license plate, was found driving without a license and possession of marijuana. "
+  },
+  {
+  team_name: "Kansas City Chiefs",
+  player: "Roy Miller",
+  position: "DT",
+  category: "Domestic Battery",
+  description: "Arrested by the Jacksonville sheriff's office for allegedly pulling his wife by the hair, tearing hair from her head and ripping her shirt. "
+  },
+  {
+  team_name: "Kansas City Chiefs",
+  player: "Demetrius Harris",
+  position: "TE",
+  category: "Drugs",
+  description: "Suspected of felony marijuana possession as passenger in car pulled over in Missouri."
+  },
+  {
+  team_name: "Chicago Bears",
+  player: "Deiondre' Hall",
+  position: "CB",
+  category: "Disorderly conduct",
+  description: "Accused of disorderly conduct, public intoxication and interference outside bar in Cedar Falls, Iowa."
+  },
+  {
+  team_name: "Chicago Bears",
+  player: "Ray McDonald",
+  position: "DT",
+  category: "Domestic violence",
+  description: "Accused of assaulting a woman while she was holding a baby in Santa Clara, Calif."
+  },
+  {
+  team_name: "Chicago Bears",
+  player: "Tim Jennings",
+  position: "CB",
+  category: "DUI",
+  description: "Pulled over for allegedly driving 99 mph in 65-mph zone in Georgia. Accused of drunken and reckless driving."
+  },
+  {
+  team_name: "Miami Dolphins",
+  player: "Rey Maualuga",
+  position: "LB",
+  category: "Battery",
+  description: "ESPN confirmed that court records in Miami-Dade County show an open battery case against Maualuga, though no other information was listed about that matter on Saturday."
+  },
+  {
+  team_name: "Miami Dolphins",
+  player: "Leon Orr",
+  position: "DT",
+  category: "Drugs",
+  description: "Pulled over for tinted windows, charged with marijuana possession near Naples, Fla. Deputies also found gun in car."
+  },
+  {
+  team_name: "Miami Dolphins",
+  player: "Derrick Shelby",
+  position: "DE",
+  category: "Trespassing",
+  description: "Police report in Fort Lauderdale says he was touching females at club without permission and refused to leave when asked."
+  }
+]
---
+++ data/teams.js
@@ -0,0 +1,162 @@
+[
+  {
+  team_name: "Denver Broncos",
+  team_conference: "AFC",
+  arrest_count: "51"
+  },
+  {
+  team_name: "Minnesota Vikings",
+  team_conference: "NFC",
+  arrest_count: "50"
+  },
+  {
+  team_name: "Cincinnati Bengals",
+  team_conference: "AFC",
+  arrest_count: "44"
+  },
+  {
+  team_name: "Jacksonville Jaguars",
+  team_conference: "AFC",
+  arrest_count: "41"
+  },
+  {
+  team_name: "Tampa Bay Buccaneers",
+  team_conference: "NFC",
+  arrest_count: "37"
+  },
+  {
+  team_name: "Tennessee Titans",
+  team_conference: "AFC",
+  arrest_count: "36"
+  },
+  {
+  team_name: "Cleveland Browns",
+  team_conference: "AFC",
+  arrest_count: "35"
+  },
+  {
+  team_name: "Indianapolis Colts",
+  team_conference: "AFC",
+  arrest_count: "35"
+  },
+  {
+  team_name: "Kansas City Chiefs",
+  team_conference: "AFC",
+  arrest_count: "34"
+  },
+  {
+  team_name: "Chicago Bears",
+  team_conference: "NFC",
+  arrest_count: "32"
+  },
+  {
+  team_name: "Miami Dolphins",
+  team_conference: "AFC",
+  arrest_count: "32"
+  },
+  {
+  team_name: "Seattle Seahawks",
+  team_conference: "NFC",
+  arrest_count: "29"
+  },
+  {
+  team_name: "San Francisco 49ers",
+  team_conference: "NFC",
+  arrest_count: "28"
+  },
+  {
+  team_name: "Baltimore Ravens",
+  team_conference: "AFC",
+  arrest_count: "28"
+  },
+  {
+  team_name: "Los Angeles Chargers",
+  team_conference: "AFC",
+  arrest_count: "27"
+  },
+  {
+  team_name: "New Orleans Saints",
+  team_conference: "NFC",
+  arrest_count: "26"
+  },
+  {
+  team_name: "Green Bay Packers",
+  team_conference: "NFC",
+  arrest_count: "26"
+  },
+  {
+  team_name: "Arizona Cardinals",
+  team_conference: "NFC",
+  arrest_count: "25"
+  },
+  {
+  team_name: "Pittsburgh Steelers",
+  team_conference: "AFC",
+  arrest_count: "25"
+  },
+  {
+  team_name: "New York Jets",
+  team_conference: "AFC",
+  arrest_count: "24"
+  },
+  {
+  team_name: "Oakland Raiders",
+  team_conference: "AFC",
+  arrest_count: "23"
+  },
+  {
+  team_name: "Washington Redskins",
+  team_conference: "NFC",
+  arrest_count: "23"
+  },
+  {
+  team_name: "Buffalo Bills",
+  team_conference: "AFC",
+  arrest_count: "22"
+  },
+  {
+  team_name: "Dallas Cowboys",
+  team_conference: "NFC",
+  arrest_count: "22"
+  },
+  {
+  team_name: "Carolina Panthers",
+  team_conference: "NFC",
+  arrest_count: "22"
+  },
+  {
+  team_name: "New York Giants",
+  team_conference: "NFC",
+  arrest_count: "21"
+  },
+  {
+  team_name: "New England Patriots",
+  team_conference: "AFC",
+  arrest_count: "21"
+  },
+  {
+  team_name: "Atlanta Falcons",
+  team_conference: "NFC",
+  arrest_count: "21"
+  },
+  {
+  team_name: "Philadelphia Eagles",
+  team_conference: "NFC",
+  arrest_count: "21"
+  },
+  {
+  team_name: "Los Angeles Rams",
+  team_conference: "NFC",
+  arrest_count: "19"
+  },
+  {
+  team_name: "Detroit Lions",
+  team_conference: "NFC",
+  arrest_count: "19"
+  },
+  {
+  team_name: "Houston Texans",
+  team_conference: "AFC",
+  arrest_count: "16"
+  }
+  ]
